a17e1e9d30b48536d2d02f07287d6152f5905326
yuval-yssak <yuval.yssak@gmail.com>
2022-08-08 20:01:48 -0400

    add notifications

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ if ("serviceWorker" in navigator) {
 }
 
 const $installButton = document.querySelector(".btn__install-app");
+const $enableNotifications = document.querySelector(
+  ".btn__enable-notifications"
+);
 
 // hide install button
 console.log("hide button");
@@ -14,6 +17,7 @@ $installButton.style.display = "none";
 
 let deferredInstallEvent = null;
 window.addEventListener("beforeinstallprompt", onPromptInstall);
+$enableNotifications.addEventListener("click", enableNotifications);
 
 function onPromptInstall(e) {
   console.log("BEFORE INSTALL PROMPT", e);
@@ -35,4 +39,19 @@ function onPromptInstall(e) {
       });
     }
   });
+}
+
+function enableNotifications() {
+  if ("Notification" in window && "serviceWorker" in navigator)
+    Notification.requestPermission().then((result) => {
+      if (result === "granted") {
+        navigator.serviceWorker.ready.then((swReg) => {
+          swReg.showNotification("Alright!", {
+            body: "Allowed notification2222s",
+            icon: "/logo-192.png",
+            badge: "/logo-96.png",
+          });
+        });
+      }
+    });
 }
