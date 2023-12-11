c30ee114051bdeca8580a583793131a882324b62
Yuval Yssak <yuval.yssak@gmail.com>
2023-12-11 16:58:10 +0200

    Update app.js

--- front-end/app.js
+++ front-end/app.js
@@ -1,15 +1,18 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("service worker registered.");
-  });
+    navigator.serviceWorker.register("/sw.js").then(() => {
+        console.log("service worker registered.");
+    });
 } else {
-  console.log("no service worker option available.");
+    console.log("no service worker option available.");
 }
 
 const $installButton = document.querySelector(".btn__install-app");
-const $enableNotifications = document.querySelector(
-  ".btn__enable-notifications"
-);
+const $enableNotifications = document.querySelector(".btn__enable-notifications");
+const $hostUrlInput = document.querySelector(".input__host_url");
+
+let hostUrl = "";
+
+$hostUrlInput.addEventListener("change", (e) => (hostUrl = e.target.value));
 
 // hide install button
 console.log("hide button");
@@ -20,101 +23,98 @@ window.addEventListener("beforeinstallprompt", onPromptInstall);
 $enableNotifications.addEventListener("click", enableNotifications);
 
 function onPromptInstall(e) {
-  console.log("BEFORE INSTALL PROMPT", e);
-  e.preventDefault();
+    console.log("BEFORE INSTALL PROMPT", e);
+    e.preventDefault();
 
-  deferredInstallEvent = e;
+    deferredInstallEvent = e;
 
-  $installButton.style.display = "block";
+    $installButton.style.display = "block";
 
-  $installButton.addEventListener("click", () => {
-    if (deferredInstallEvent) {
-      deferredInstallEvent.prompt();
+    $installButton.addEventListener("click", () => {
+        if (deferredInstallEvent) {
+            deferredInstallEvent.prompt();
 
-      deferredInstallEvent.userChoice().then((result) => {
-        if (result === "accepted") console.log("user agreed to install");
-        else console.log("user declined to install");
+            deferredInstallEvent.userChoice().then((result) => {
+                if (result === "accepted") console.log("user agreed to install");
+                else console.log("user declined to install");
 
-        deferredInstallEvent = null;
-      });
-    }
-  });
+                deferredInstallEvent = null;
+            });
+        }
+    });
 }
 
 function enableNotifications() {
-  if ("Notification" in window && "serviceWorker" in navigator)
-    Notification.requestPermission().then((result) => {
-      if (result === "granted") {
-        navigator.serviceWorker.ready.then((swReg) => {
-          configurePushSubscription(swReg);
+    if ("Notification" in window && "serviceWorker" in navigator)
+        Notification.requestPermission().then((result) => {
+            if (result === "granted") {
+                navigator.serviceWorker.ready.then((swReg) => {
+                    configurePushSubscription(swReg);
+                });
+            }
         });
-      }
-    });
 }
 
 function displayConfirmaNotification(swReg) {
-  swReg.showNotification("Alright!", {
-    body: "You have enabled notifications",
-    icon: "/logo-192.png",
-    badge: "/logo-96.png",
-    actions: [
-      {
-        action: "confirm-slug",
-        title: "Confirm",
+    swReg.showNotification("Alright!", {
+        body: "You have enabled notifications",
         icon: "/logo-192.png",
-      },
-      {
-        action: "cancel-slug",
-        title: "Cancel",
-        icon: "/logo-192.png",
-      },
-    ],
-  });
+        badge: "/logo-96.png",
+        actions: [
+            {
+                action: "confirm-slug",
+                title: "Confirm",
+                icon: "/logo-192.png",
+            },
+            {
+                action: "cancel-slug",
+                title: "Cancel",
+                icon: "/logo-192.png",
+            },
+        ],
+    });
 }
 
 async function configurePushSubscription(swReg) {
-  const vapidPublicKey =
-    "BAX_0TF8ANYwXuAIxcgcmjokFlq2XlT3HHWUyEQTy7Aze2_U_tRnD2NwDLw0hZrWm77C1765FOro81lLn6O8PuU";
-  const convertedKey = convertDataURIToBinary(vapidPublicKey);
-  if (!("serviceWorker" in navigator)) return;
+    const vapidPublicKey = "BAX_0TF8ANYwXuAIxcgcmjokFlq2XlT3HHWUyEQTy7Aze2_U_tRnD2NwDLw0hZrWm77C1765FOro81lLn6O8PuU";
+    const convertedKey = convertDataURIToBinary(vapidPublicKey);
+    if (!("serviceWorker" in navigator)) return;
 
-  const subscription = await swReg.pushManager.getSubscription();
+    const subscription = await swReg.pushManager.getSubscription();
 
-  if (subscription === null) {
-    // create a new subscription
-    const newSubscription = await swReg.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: convertedKey,
-    });
+    if (subscription === null) {
+        // create a new subscription
+        const newSubscription = await swReg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: convertedKey,
+        });
 
-    const response = await fetch("http://localhost:4000/subscribe", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(newSubscription),
-    });
-    if (response.ok) displayConfirmaNotification(swReg);
-    else alert("could not push subscription to server");
-  } else {
-    // we have a subscription
+        const response = await fetch(`${hostUrl}/subscribe`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(newSubscription),
+        });
+        if (response.ok) displayConfirmaNotification(swReg);
+        else alert("could not push subscription to server");
+    } else {
+        // we have a subscription
 
-    console.log("subscription already exists");
-  }
+        console.log("subscription already exists");
+    }
 }
 
 function convertDataURIToBinary(base64string) {
-  const padding = "=".repeat((4 - (base64string.length % 4)) % 4);
-  const base64 = (base64string + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
+    const padding = "=".repeat((4 - (base64string.length % 4)) % 4);
+    const base64 = (base64string + padding).replace(/\-/g, "+").replace(/_/g, "/");
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
 
-  for (let i = 0; i < rawData.length; i++) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+    for (let i = 0; i < rawData.length; i++) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
